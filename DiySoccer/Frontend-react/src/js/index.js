346551f4715939bef09a02da39ce14bfbe878cc1
Sergei_Golberg <golberg.sergei@gmail.com>
2023-06-21 10:31:07 +0400

    tournaments displaying

--- DiySoccer/Frontend-react/src/js/index.js
+++ DiySoccer/Frontend-react/src/js/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { data } from './data.js';
 
-const root = createRoot(document.getElementById('league'));
-
+const LeagueRoot = createRoot(document.getElementById('league'));
+const tournamentsRoot = createRoot(document.getElementById('tournaments'));
 
 const leagueElements = [];
-console.log(data);
+//console.log(data);
 for (const index in data.leagues) {
     const logo = (
         <img
@@ -36,4 +36,36 @@ for (const index in data.leagues) {
     
 };
 
-root.render(leagueElements);
+const tournamentElements = [];
+//console.log(data);
+for (const index in data.tournaments) {
+    const logo = (
+        <img
+            className='dashboard-list-item-logo img-70-70'
+            src={data.tournaments[index].logo}
+            alt={data.tournaments[index].alt}
+        />
+    );
+    const name = data.tournaments[index].name;
+    const description = data.tournaments[index].description;
+
+    const tournamentElement = (
+        <div className="col-sm-4">
+            <a className="dashboard-list-item" href="#">
+                <div className="dashboard-list-item-sub">
+                    {logo}
+                </div>
+                <div className="dashboard-list-item-sub">
+                    <div className="dashboard-list-item_name">{name}</div>
+                    <div className="dashboard-list-item_description">{description}</div>
+                </div>
+            </a>
+        </div>
+    );
+
+    tournamentElements.push(tournamentElement);
+    
+};
+
+LeagueRoot.render(leagueElements);
+tournamentsRoot.render(tournamentElements);
