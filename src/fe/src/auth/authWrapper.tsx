f77af68b42333fa92287e36b45fdd4560b27faad
Anton markov <amrkov5@gmail.com>
2025-02-06 11:57:14 +0200

    refactor: refactor authwrapper to check ids

--- src/fe/src/auth/authWrapper.tsx
+++ src/fe/src/auth/authWrapper.tsx
@@ -27,6 +27,7 @@ export default function AuthWrapper({ children }: { children: JSX.Element }) {
       return children;
     }
     if (PARTIALLY_PROTECTED_ROUTES.includes(path[1]) && !params.id) {
+      // if (PARTIALLY_PROTECTED_ROUTES.includes(path[1]) && !params.id) {
       return children;
     }
     return <Navigate to="/signin" replace />;
@@ -41,6 +42,7 @@ export default function AuthWrapper({ children }: { children: JSX.Element }) {
         return children;
       }
       if (PARTIALLY_PROTECTED_ROUTES.includes(path[1]) && !params.id) {
+        // if (PARTIALLY_PROTECTED_ROUTES.includes(path[1]) && !params.id) {
         return children;
       }
 
